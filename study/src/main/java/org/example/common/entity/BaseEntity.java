78ceeff142dc7fb7d618436ffe9989fe0449846a
Azin <setiguy@gachon.ac.kr>
2025-08-12 12:52:01 +0900

    feat/#1: 중간 커밋

---
+++ study/src/main/java/org/example/common/entity/BaseEntity.java
@@ -0,0 +1,40 @@
+package org.example.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ study/src/main/java/org/example/domain/member/entity/Member.java
@@ -0,0 +1,11 @@
+package org.example.domain.member.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import org.example.common.entity.BaseEntity;
+
+@Entity
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+}
